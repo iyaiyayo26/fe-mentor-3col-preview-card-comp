50e875b1b72f31eb8450e45a5b3190b511ca5a06
iyaiyayo26 <fahiyanmr26@gmail.com>
2022-11-15 15:46:34 +0700

    membetulkan heading, button

--- src/App.js
+++ src/App.js
@@ -3,38 +3,39 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="p-10 lg:p-20">
-        <div className=" w-60 rounded-md mx-auto md:flex md:w-full md:h-[400px] lg:h-[500px] overflow-hidden">
-          {/* card 1 */}
-          <div className="bg-bright-orange">
-            <div className="px-8 py-8 lg:px-14 lg:py-10">
-              <img src="./images/icon-sedans.svg" alt="svg-icon" className="w-10 lg:w-20" />
-              <p className="text-white uppercase my-6 lg:my-12 font-big-shoulders text-4xl">sedans</p>
-              <p className="text-white mb-5 lg:mb-10 lg:mr-10 font-lexend-deca text-sm text-transparent-white">Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
-              <button className="bg-white px-6 py-3 rounded-full text-bright-orange font-lexend-deca text-xs hover:cursor-pointer hover:bg-bright-orange hover:text-white hover:ring-1 hover:ring-inset hover:ring-white">Learn More</button>
-            </div>
-          </div>
+      <div className="flex flex-col items-stretch rounded-lg mx-20 md:flex-row overflow-hidden">
+        <section className="p-8 bg-bright-orange">
+          <img src="./images/icon-sedans.svg" alt="sedans-icon" />
+          <h1 className="uppercase my-8 text-white font-big-shoulders text-3xl">Sedans</h1>
+          <p className="text-transparent-white font-lexend-deca text-sm">Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
+          <button className="mt-7 md:mt-12">
+            <a href="#" className="bg-white rounded-full px-5 py-2 font-lexend-deca text-sm text-bright-orange hover:text-white hover:bg-bright-orange hover:border hover:border-white">
+              Learn More
+            </a>
+          </button>
+        </section>
 
-          {/* card 2 */}
-          <div className="bg-dark-cyan">
-            <div className="px-8 py-8 lg:px-14 lg:py-10">
-              <img src="./images/icon-suvs.svg" alt="svg-icon" className="w-10 lg:w-20" />
-              <p className="text-white uppercase my-6 lg:my-12 font-big-shoulders text-4xl">suvs</p>
-              <p className="text-white mb-5 lg:mb-10 lg:mr-10 font-lexend-deca text-sm text-transparent-white">Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.</p>
-              <button className="bg-white px-6 py-3 rounded-full text-dark-cyan font-lexend-deca text-xs hover:cursor-pointer hover:bg-dark-cyan hover:text-white hover:ring-1 hover:ring-inset hover:ring-white">Learn More</button>
-            </div>
-          </div>
+        <section className="p-8 bg-dark-cyan ">
+          <img src="./images/icon-suvs.svg" alt="suvs-icon" />
+          <h1 className="uppercase my-8 text-white font-big-shoulders text-3xl">suvs</h1>
+          <p className="text-transparent-white font-lexend-deca text-sm">Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.</p>
+          <button className="mt-7 md:mt-12">
+            <a href="#" className="bg-white rounded-full px-5 py-2 font-lexend-deca text-sm text-dark-cyan hover:text-white hover:bg-dark-cyan hover:border hover:border-white">
+              Learn More
+            </a>
+          </button>
+        </section>
 
-          {/* card 3 */}
-          <div className="bg-very-dark-cyan">
-            <div className="px-8 py-8 lg:px-14 lg:py-10">
-              <img src="./images/icon-luxury.svg" alt="svg-icon" className="w-10 lg:w-20" />
-              <p className="text-white uppercase my-6 lg:my-12 font-big-shoulders text-4xl">luxury</p>
-              <p className="text-white mb-5 lg:mb-10 lg:mr-10 font-lexend-deca text-sm text-transparent-white">Cruise in the best car brands without the bloated prices. Enjoy the enchanted confort of a luxury rental and arrive in style.</p>
-              <button className="bg-white px-6 py-3 rounded-full text-very-dark-cyan font-lexend-deca text-xs hover:cursor-pointer hover:bg-very-dark-cyan hover:text-white hover:ring-1 hover:ring-inset hover:ring-white">Learn More</button>
-            </div>
-          </div>
-        </div>
+        <section className="p-8 bg-very-dark-cyan">
+          <img src="./images/icon-luxury.svg" alt="luxury-icon" />
+          <h1 className="uppercase my-8 text-white font-big-shoulders text-3xl">luxury</h1>
+          <p className="text-transparent-white font-lexend-deca text-sm">Cruise in the best car brands without the bloated prices. Enjoy the enchanced comfort of a luxury rental and arrive in style.</p>
+          <button className="mt-7 md:mt-12">
+            <a href="#" className="bg-white rounded-full px-5 py-2 font-lexend-deca text-sm text-very-dark-cyan hover:text-white hover:bg-very-dark-cyan hover:border hover:border-white">
+              Learn More
+            </a>
+          </button>
+        </section>
       </div>
     </>
   );
